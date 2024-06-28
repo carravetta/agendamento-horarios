178b8aa531c096f5886caab00fa7f0b23e5dd9e6
carravetta <lucas_carravetta@hotmail.com>
2024-06-27 22:22:34 -0300

    node

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, resp)=>{
-    resp.status(200).send("../index");
+    resp.status(200).sendFile(path.join(__dirname, `/view`, `index.html`));
 });
 
 app.listen(3000, ()=>{
     console.log("Servidor rodando");
 })
+
